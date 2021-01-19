3be70d02ff21a4192c9d524c021aa309067894a0
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-01-19 22:33:34 +0200

    create search fnc

---
+++ src/js/countrySearch.js
@@ -0,0 +1,22 @@
+import 'lodash';
+
+import fetchCountries from './fetchCountries';
+import updateCountriesMarkup from './updateCountriesMarkup';
+
+const searchInput = document.querySelector('.search-country');
+
+const debouncedCallback = _.debounce(event => {
+  // console.log(event.target.value);
+
+  fetchCountries(event.target.value)
+    .then(data => {
+      // console.log(data.length);
+      updateCountriesMarkup(data);
+    })
+    .catch(error => {
+      // resultRef.innerHTML = '';
+      // notice(noticeOptions);
+    });
+}, 500);
+
+searchInput.addEventListener('input', debouncedCallback);
