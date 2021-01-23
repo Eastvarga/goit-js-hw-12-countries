f4687dc219440ae0a1855251254cad171d5e4515
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-01-23 12:26:17 +0200

    some refactoring update

--- src/js/updateCountriesMarkup.js
+++ src/js/updateCountriesMarkup.js
@@ -8,10 +8,9 @@ import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/mobile/dist/PNotifyMobile.css';
 
 import {
-  notice,
+  success,
   error,
   defaultModules,
-  defaults,
   Stack,
 } from '@pnotify/core/dist/PNotify.js';
 import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
@@ -25,37 +24,49 @@ const myStack = new Stack({
   context: document.body,
 });
 
-const noticeOptions = {
-  ...defaults,
+// const noticeOptions = {
+//   ...defaults,
+//   title: 'Sorry',
+//   text: 'Too many matches found. Please enter a more specific query!',
+//   module: defaultModules,
+//   stack: myStack,
+//   delay: 1000,
+// };
+const successOption = {
+  title: 'Found',
+  text: 'You are amaizing!',
+  delay: 2000,
+  stack: myStack,
+};
+
+const errorOption = {
   title: 'Sorry',
   text: 'Too many matches found. Please enter a more specific query!',
   module: defaultModules,
   stack: myStack,
-  delay: 1000,
+  delay: 2000,
 };
-
 //
 
 const resultRef = document.querySelector('.result');
 
 function updateCountriesMarkup(countriesData) {
   resultRef.innerHTML = '';
-  if (countriesData.length == 1) {
-    const markup = countryData(countriesData);
-    resultRef.insertAdjacentHTML('beforeend', markup);
-    return;
-  }
-  if (countriesData.length > 1 && countriesData.length <= 10) {
-    const markup = countryList(countriesData);
-    resultRef.insertAdjacentHTML('beforeend', markup);
-    return;
-  }
-  if (countriesData.length > 10) {
-    notice(noticeOptions);
-    return;
-  }
-  if (countriesData.length === undefined) {
-    error({ ...noticeOptions, text: 'Some error occured!!!' });
+  if (countriesData.length >= 1) {
+    if (countriesData.length > 1 && countriesData.length <= 10) {
+      const markup = countryList(countriesData);
+      resultRef.insertAdjacentHTML('beforeend', markup);
+      return;
+    }
+    if (countriesData.length === 1) {
+      const markup = countryData(countriesData);
+      resultRef.insertAdjacentHTML('beforeend', markup);
+      success(successOption);
+      return;
+    }
+    error(errorOption);
+  } else {
+    error(errorOption);
   }
 }
 
